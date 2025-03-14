aa5971c7985a1df9e8f64fdbceeae56d8735f85e
kasra Gordini <kasragordini@gmail.com>
2025-03-14 01:13:31 -0400

    conditional list comprehension

--- conditional_list_comprehension.py
+++ conditional_list_comprehension.py
@@ -6,3 +6,8 @@ new_fellowship = [member for member in fellowship if len(member)>=7]
 
 print(new_fellowship)
 
+#############################
+
+new_fellowship = [member if len(member) >= 7 else "" for member in fellowship]
+
+print(new_fellowship)
